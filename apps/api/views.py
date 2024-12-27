ff8e3ba3a2ff668d3687bc50a21e0e0a7b079369
l0n3r <mirshohid1214@gmail.com>
2024-12-27 05:05:11 +0500

    chore: run makemigrations

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from ..files.models import File
+from ..folders.models import Folder
