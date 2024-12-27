9c84abd2f039a7e1d0b40f8c373f51893c08b57b
l0n3r <mirshohid1214@gmail.com>
2024-12-27 08:52:33 +0500

    feat: add input serializer for Folder

--- apps/folders/serializers.py
+++ apps/folders/serializers.py
@@ -6,3 +6,29 @@ class FolderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Folder
         fields = ('name', 'parent')
+
+
+class FolderInputSerializer(serializers.ModelSerializer):
+    folderId = serializers.SerializerMethodField
+    folderName = serializers.SerializerMethodField
+    createdAt = serializers.SerializerMethodField
+    parentFolderId = serializers.SerializerMethodField
+
+    class Meta:
+        model = Folder
+        fields = (
+            'folderId', 'folderName',
+            'createdAt', 'parentFolderId',
+        )
+
+    def get_folderId(self, obj):
+        return obj.id
+
+    def get_folderName(self, obj):
+        return obj.name
+
+    def get_createdAt(self, obj):
+        return obj.created_at
+
+    def get_parentFolderId(self, obj):
+        return obj.parent.id
