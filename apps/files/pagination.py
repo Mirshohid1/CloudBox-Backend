1e9231976065b93f350c391a13bf43b9acb8832b
l0n3r <mirshohid1214@gmail.com>
2024-12-27 08:39:46 +0500

    feat: add custom pagination

---
+++ apps/files/pagination.py
@@ -0,0 +1,16 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "files": data,
+            "totalFiles": self.page.paginator.count,
+            "currentPage": self.page.number,
+            "totalPages": self.page.paginator.num_pages,
+        })
