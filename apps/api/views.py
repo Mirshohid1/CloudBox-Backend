c63e807d2e542f9ba24d17831a03fc3d1dbcb211
l0n3r <mirshohid1214@gmail.com>
2024-12-27 05:12:07 +0500

    feat: add FileViewSet

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from ..files.models import File
+from ..files.serializers import FileSerializer
 from ..folders.models import Folder
 from ..folders.serializers import FolderSerializer
 
@@ -7,3 +8,8 @@ from ..folders.serializers import FolderSerializer
 class FolderViewSet(ModelViewSet):
     queryset = Folder.objects.all()
     serializer_class = FolderSerializer
+
+
+class FileViewSet(ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
