a0a57e1af5a8c6c3792044fd532e2ead7f8b89db
l0n3r <mirshohid1214@gmail.com>
2024-12-29 03:04:31 +0500

    feat: update logical validation in User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,39 +6,29 @@ from services.formatting import data_formatting
 
 
 class User(AbstractUser):
-    email = models.EmailField(
-        _("email address"),
-        unique=True,
-    )
-    storage_limit = models.PositiveIntegerField(
-        _("storage limit"),
-        default=10240,
-    )
-    used_storage = models.PositiveIntegerField(
-        _("used storage"),
-        default=0,
-    )
+    email = models.EmailField(_("email address"), unique=True)
+    storage_limit = models.PositiveIntegerField(_("storage limit"), default=10240)
+    used_storage = models.PositiveIntegerField(_("used storage"), default=0)
+
+    def validate_unique_username(self):
+        if User.objects.filter(username=self.username).exclude(pk=self.pk).exists():
+            raise ValidationError({'username': _("The username already exists.")})
+
+    def validate_unique_email(self):
+        if User.objects.filter(email=self.email).exclude(pk=self.pk).exists():
+            raise ValidationError({"email": _("The email address is already in use.")})
 
     def clean(self):
         super().clean()
-
-        self.first_name = data_formating(self.first_name, True)
-        self.last_name = data_formating(self.last_name, True)
-        self.username = data_formating(self.username)
-        self.email = data_formating(self.email)
+        self.first_name = data_formatting(self.first_name, True)
+        self.last_name = data_formatting(self.last_name, True)
+        self.username = data_formatting(self.username, is_required_field=True)
+        self.email = data_formatting(self.email, is_required_field=True)
+        self.validate_unique_username()
+        self.validate_unique_email()
 
     def save(self, *args, **kwargs):
-        self.clean()
-
-        if User.objects.filter(username=self.username).exclude(pk=self.pk).exists():
-            raise ValidationError({'username': "The username already exists."})
-        if User.objects.filter(email=self.email).exclude(pk=self.pk).exists():
-            raise ValidationError({'email': "The email address is already in use."})
-        if ' ' in self.first_name:
-            raise ValidationError({'first_name': "Spaces are not allowed in the first name."})
-        if ' ' in self.last_name:
-            raise ValidationError({'last_name': "Spaces are not allowed in the last name."})
-
+        self.full_clean()
         super().save(*args, **kwargs)
 
     def __str__(self):
--- services/formatting.py
+++ services/formatting.py
@@ -1,10 +1,11 @@
 from django.core.exceptions import ValidationError
 
 
-def data_formatting(value: str, is_name=False) -> str:
+def data_formatting(value: str, is_name=False, is_required_field=False) -> str:
     if value and value.strip():
         value = value.strip().capitalize() if is_name else value.strip()
     else:
-        raise ValidationError("This field is required and should not be just spaces.")
+        if is_required_field:
+            raise ValidationError("This field is required and should not be just spaces.")
 
     return value
