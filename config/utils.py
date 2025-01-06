759f6a0e4384ebb4799598fcfa6509d467a6644a
l0n3r <mirshohid1214@gmail.com>
2025-01-06 05:47:34 +0500

    refactor: write docstring for data_formatting func

--- config/utils.py
+++ config/utils.py
@@ -1,7 +1,16 @@
 from django.core.exceptions import ValidationError
 
 
-def data_formatting(value, is_name=False, is_required_field=True):
+def data_formatting(value: str, is_name: bool = False, is_required_field: bool = True):
+    """
+    Formats the provided string value by stripping whitespace and capitalizing it if specified as a name.
+
+    :param value: The string value to be formatted.
+    :param is_name: If True, the value will be capitalized. Default is False.
+    :param is_required_field: If True, raises ValidationError for empty or whitespace-only values. Default is True.
+    :return: The formatted string value.
+    :raises ValidationError: If is_required_field is True and the value is empty or contains only whitespace.
+    """
     if value and value.strip():
         return value.strip().capitalize() if is_name else value.strip()
     else:
